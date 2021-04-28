8d859e714547faa8db4f0ef43f824490d7305ad4
njm7162 <njm7162@autuni.ac.nz>
2021-04-28 20:10:19 +1200

    set up to retrieve all the books

--- backend/routes/api/articles.js
+++ backend/routes/api/articles.js
@@ -1,23 +1,26 @@
 
 const express = require('express');
 const router = express.Router();
+var cors = require('cors');
+
 
 //Load Article model
 const Article = require('../../models/Article');
 
 router.get('/test', (req, res) => res.send('articles route testing!'));
 
-router.get('/:id', (req, res) => {
-    Article.findById(req.params.id)
-      .then(article => res.json(article))
-      .catch(err => res.status(404).json({ noarticlefound: 'No Book found' }));
-  });
+router.get('/', (req, res) => {
+  Article.find()
+    .then(articles => res.json(articles))
+    .catch(err => res.status(404).json({ nobooksfound: 'No Books found' }));
+});
 
-  router.post('/', (req, res) => {
-    Article.create(req.body)
-      .then(book => res.json({ msg: 'Book added successfully' }))
-      .catch(err => res.status(400).json({ error: 'Unable to add this book' }));
-  });
-  
+
+router.post('/addNewArticle', (req, res) => {
+  Article.create(req.body)
+    .then(article => res.json({ msg: 'Book added successfully' }))
+    .catch(err => res.status(400).json({ error: 'Unable to add this book' }));
+});
+
 
 module.exports = router;
