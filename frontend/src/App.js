ffc9d8b1ab2fdac42fac912e42878e41f4a4127d
HaseebQammarCheema <haseebqammarcheema1999@gmail.com>
2021-04-28 13:01:55 +1200

    working search bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
-import Search from './search';
 
 import CreateBook from './components/CreateBook';
 import ShowBookList from './components/ShowBookList';
 import ShowBookDetails from './components/ShowBookDetails';
 import UpdateBookInfo from './components/UpdateBookInfo';
+import search from './components/search';
 
 class App extends Component {
   render() {
@@ -17,17 +17,12 @@ class App extends Component {
           <Route path='/create-book' component={CreateBook} />
           <Route path='/edit-book/:id' component={UpdateBookInfo} />
           <Route path='/show-book/:id' component={ShowBookDetails} />
+          <Route path='/search-bar' component={search} />
         </div>
       </Router>
     );
   }
 }
 
-const App = () => {
-  return (
-      <Search />
-  );
-}
 
-export default App;
 export default App;
